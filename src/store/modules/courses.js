1a71f22d870094cc25268afee9fb63bf7af4748b
owner <weihuang0313@gmail.com>
2020-07-08 19:21:47 -0400

    enable debugger + integrate API support for general course code (MATH, SCIENCE, LAB)

--- src/store/modules/courses.js
+++ src/store/modules/courses.js
@@ -4,51 +4,86 @@ import {CourseInfo, CourseRequirement} from '../../models/courseModel'
 
 const backend_api = "http://127.0.0.1:8000"
 
+
 // Fetch course information of a single course code (eg MATH 239 or PHYS 300-)
 async function parseRequirement(courseCode) {
-    //handle the exceptions [e.g. NON-MATH]
-    if (courseCode == "NON-MATH") {
-        return [new CourseInfo({
-            course_name: "Course not offered by the Faculty of Math",
-            course_code: "NON-MATH"
-        })]
+    var hasNumber = /\d/;
+    if (!hasNumber.test(courseCode)){
+        //handle the exceptions [e.g. NON-MATH]
+            
+        if (courseCode == "NON-MATH") {
+            return [new CourseInfo({
+                course_name: "Course not offered by the Faculty of Math",
+                course_code: "NON-MATH"
+            })]
+        }
+        else if (courseCode.includes("Elective")) {
+            return [new CourseInfo({
+                course_name: "Elective course - Temp Place Holder",
+                course_code: courseCode
+            })]
+        }
+        else{
+            const response = await axios.get(backend_api + "/api/course-info/filter", {
+                params: {
+                    start: 0,
+                    end: 499,
+                    code: courseCode,
+                }
+            })
+            return response.data.map(element => { return new CourseInfo(element) });
+        }
     }
-
-
-    var split;
-    if (courseCode[courseCode.length - 1] === "-") {
-        // Handles X00's case, eg PHYS 300-
-        split = courseCode.split(" ");
-        const response = await axios.get(backend_api + "/api/course-info/filter", {
-            params: {
-                start: Number(split[1].slice(0, -1)),
-                end: Number(split[1].slice(0, -1)) + 99,
-                code: split[0],
+    else{
+        var split;
+        if (courseCode[courseCode.length - 1] === "-") {
+            // Handles X00's case, eg PHYS 300-
+            let response;
+            split = courseCode.split(" ");
+            
+            if(split[1] === "LAB"){
+        
+                response = await axios.get(backend_api + "/api/course-info/filter", {
+                    params: {
+                        start: Number(split[2].slice(0, -1)),
+                        end: Number(split[2].slice(0, -1)) + 99,
+                        code: split[0] + " " + split[1],
+                    }
+                })
             }
-        })
-        // console.log("1 " +  courseCode, response.data)
-        return response.data;
-    } else if (courseCode.split("-").length === 2 && courseCode.split("-")[0].length > 0 && courseCode.split("-")[1].length > 0) {
-        // Handles range case, eg CS 440-CS 498
-        split = courseCode.split("-");
-        const response = await axios.get(backend_api + "/api/course-info/filter", {
-            params: {
-                start: Number(split[0].split(" ")[1]),
-                end: Number(split[1].split(" ")[1]),
-                code: split[0].split(" ")[0],
+            else{
+                response = await axios.get(backend_api + "/api/course-info/filter", {
+                    params: {
+                        start: Number(split[1].slice(0, -1)),
+                        end: Number(split[1].slice(0, -1)) + 99,
+                        code: split[0],
+                    }
+                })
             }
-        })
-        // console.log("2 " +  courseCode, response.data)
-        return response.data.map(element => { return new CourseInfo(element) });
-    } else {
-        // Handles normal course case, ege MATH 239
-        const response = await axios.get(backend_api + "/api/course-info/get", {
-            params: {
-                pk: courseCode,
-            }
-        });
-        // console.log("3 " +  courseCode, response.data)
-        return [new CourseInfo(response.data)];
+            // console.log("1 " +  courseCode, response.data)
+            return response.data;
+        } else if (courseCode.split("-").length === 2 && courseCode.split("-")[0].length > 0 && courseCode.split("-")[1].length > 0) {
+            // Handles range case, eg CS 440-CS 498
+            split = courseCode.split("-");
+            const response = await axios.get(backend_api + "/api/course-info/filter", {
+                params: {
+                    start: Number(split[0].split(" ")[1]),
+                    end: Number(split[1].split(" ")[1]),
+                    code: split[0].split(" ")[0],
+                }
+            })
+            // console.log("2 " +  courseCode, response.data)
+            return response.data.map(element => { return new CourseInfo(element) });
+        } else {
+            // Handles normal course case, ege MATH 239
+            const response = await axios.get(backend_api + "/api/course-info/get", {
+                params: {
+                    pk: courseCode,
+                }
+            });
+            // console.log("3 " +  courseCode, response.data)
+            return [new CourseInfo(response.data)];
+        }
     }
 }
 
@@ -83,10 +118,12 @@ const actions = {
                 number_of_courses: requirement.number_of_courses,
                 major: [getters.chosenMajor[0]],
             }
+
             // Split the requirement into its individual courses and parse each of them
             var required_courses = requirement.course_codes.split(/,\s|\sor\s/)
             
-            console.log(required_courses)
+            
+           
             for (var course of required_courses) {
                 parsed_requirement.course_choices = parsed_requirement.course_choices.concat(await parseRequirement(course))
             }
