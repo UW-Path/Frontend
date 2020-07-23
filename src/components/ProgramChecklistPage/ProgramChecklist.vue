be8aa3983773d697f5bebb13a8dd81cfd4fef57f
Adrian Santo Pasqualini <aspasqua@edu.uwaterloo.ca>
2020-07-22 22:25:58 -0400

    Make Checklist Two Columns

--- src/components/ProgramChecklistPage/ProgramChecklist.vue
+++ src/components/ProgramChecklistPage/ProgramChecklist.vue
@@ -2,9 +2,11 @@
     <div class="program-checklist">
         <div class="checklist-section">
             <p class="checklist-title">All Degree Requirements</p>
-            <div class="requirements-list" v-for="requirement in checklistRequirements" :key="requirement.id">
-                <v-checkbox v-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1" class="requirement-checkbox" :label="requirement.number_of_courses + ' of ' + requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
-                <v-checkbox v-else class="requirement-checkbox" :label="requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
+            <div class="requirements-list">
+                <div class="requirements-list-item" v-for="requirement in checklistRequirements" :key="requirement.id">
+                    <v-checkbox v-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1" class="requirement-checkbox" :label="requirement.number_of_courses + ' of ' + requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
+                    <v-checkbox v-else class="requirement-checkbox" :label="requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
+                </div>
             </div>
         </div>
     </div>
@@ -35,6 +37,7 @@ export default {
 .checklist-title {
     font-size: 18px;
     font-weight: 500;
+    text-align: left;
 }
 
 .program-checklist {
@@ -50,10 +53,16 @@ export default {
     overflow-y: scroll;
 }
 
-.requirements-list {
+.requirements-list-item {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    flex: 1 0 50%;
+}
+
+.requirements-list {
+    display: flex;
+    flex-wrap: wrap;
 }
 
 .requirement-checkbox {
