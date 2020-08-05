cff75dec02010756547e3a0679ce6ca154ed74a3
Adrian Santo Pasqualini <aspasqua@edu.uwaterloo.ca>
2020-08-05 00:11:15 -0400

    Shrink Checklist Items and Add Tooltip

--- src/components/ProgramChecklistPage/ProgramChecklist.vue
+++ src/components/ProgramChecklistPage/ProgramChecklist.vue
@@ -5,8 +5,35 @@
                 <p class="checklist-title">Major Requirements</p>
                 <div class="requirements-list">
                     <div class="requirements-list-item" v-for="requirement in checklistMajorRequirements" :key="requirement.id">
-                        <v-checkbox v-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1" class="requirement-checkbox" :label="requirement.number_of_courses + ' of ' + requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
-                        <v-checkbox v-else class="requirement-checkbox" :label="requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
+                        <v-tooltip top v-if="(requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1) && requirement.course_codes.length > max_checklist_length" open-delay="300">
+                            <template v-slot:activator="{ on, attrs }">
+                                <div class="requirement-list-item-inner" v-bind="attrs" v-on="on">
+                                    <v-checkbox
+                                            class="requirement-checkbox"
+                                            color="primary"
+                                            :input-value="requirement.met">
+                                    </v-checkbox>
+                                    <p>{{requirement.number_of_courses + ' of ' + requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                                </div>
+                            </template>
+                            <span>{{requirement.course_codes}}</span>
+                        </v-tooltip>
+                        <div class="requirement-list-item-inner" v-bind="attrs" v-on="on" v-else-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1">
+                            <v-checkbox
+                                    class="requirement-checkbox"
+                                    color="primary"
+                                    :input-value="requirement.met">
+                            </v-checkbox>
+                            <p>{{requirement.number_of_courses + ' of ' + requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                        </div>
+                        <div v-else class="requirement-list-item-inner" v-bind="attrs" v-on="on">
+                            <v-checkbox
+                                class="requirement-checkbox"
+                                color="primary"
+                                :input-value="requirement.met">
+                            </v-checkbox>
+                            <p>{{requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -14,8 +41,35 @@
                 <p class="checklist-title">Minor Requirements</p>
                 <div class="requirements-list">
                     <div class="requirements-list-item" v-for="requirement in checklistMinorRequirements" :key="requirement.id">
-                        <v-checkbox v-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1" class="requirement-checkbox" :label="requirement.number_of_courses + ' of ' + requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
-                        <v-checkbox v-else class="requirement-checkbox" :label="requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
+                        <v-tooltip top v-if="(requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1) && requirement.course_codes.length > max_checklist_length" open-delay="300">
+                            <template v-slot:activator="{ on, attrs }">
+                                <div class="requirement-list-item-inner" v-bind="attrs" v-on="on">
+                                    <v-checkbox
+                                            class="requirement-checkbox"
+                                            color="primary"
+                                            :input-value="requirement.met">
+                                    </v-checkbox>
+                                    <p>{{requirement.number_of_courses + ' of ' + requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                                </div>
+                            </template>
+                            <span>{{requirement.course_codes}}</span>
+                        </v-tooltip>
+                        <div class="requirement-list-item-inner" v-bind="attrs" v-on="on" v-else-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1">
+                            <v-checkbox
+                                    class="requirement-checkbox"
+                                    color="primary"
+                                    :input-value="requirement.met">
+                            </v-checkbox>
+                            <p>{{requirement.number_of_courses + ' of ' + requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                        </div>
+                        <div v-else class="requirement-list-item-inner" v-bind="attrs" v-on="on">
+                            <v-checkbox
+                                class="requirement-checkbox"
+                                color="primary"
+                                :input-value="requirement.met">
+                            </v-checkbox>
+                            <p>{{requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -23,8 +77,35 @@
                 <p class="checklist-title">Option Requirements</p>
                 <div class="requirements-list">
                     <div class="requirements-list-item" v-for="requirement in checklistOptionRequirements" :key="requirement.id">
-                        <v-checkbox v-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1" class="requirement-checkbox" :label="requirement.number_of_courses + ' of ' + requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
-                        <v-checkbox v-else class="requirement-checkbox" :label="requirement.course_codes" color="green" :input-value="requirement.met"></v-checkbox>
+                        <v-tooltip top v-if="(requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1) && requirement.course_codes.length > max_checklist_length" open-delay="300">
+                            <template v-slot:activator="{ on, attrs }">
+                                <div class="requirement-list-item-inner" v-bind="attrs" v-on="on">
+                                    <v-checkbox
+                                            class="requirement-checkbox"
+                                            color="primary"
+                                            :input-value="requirement.met">
+                                    </v-checkbox>
+                                    <p>{{requirement.number_of_courses + ' of ' + requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                                </div>
+                            </template>
+                            <span>{{requirement.course_codes}}</span>
+                        </v-tooltip>
+                        <div class="requirement-list-item-inner" v-bind="attrs" v-on="on" v-else-if="requirement.number_of_courses > 1 || requirement.course_codes.split(',').length > 1">
+                            <v-checkbox
+                                    class="requirement-checkbox"
+                                    color="primary"
+                                    :input-value="requirement.met">
+                            </v-checkbox>
+                            <p>{{requirement.number_of_courses + ' of ' + requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                        </div>
+                        <div v-else class="requirement-list-item-inner" v-bind="attrs" v-on="on">
+                            <v-checkbox
+                                class="requirement-checkbox"
+                                color="primary"
+                                :input-value="requirement.met">
+                            </v-checkbox>
+                            <p>{{requirement.course_codes.slice(0, max_checklist_length) + (requirement.course_codes.length > max_checklist_length ? '...' : '')}}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -39,6 +120,7 @@ export default {
     data() {
         return {
             test: true,
+            max_checklist_length: 40,
         }
     },
     methods: {
@@ -54,6 +136,12 @@ export default {
 }
 </script>
 <style scoped>
+.requirement-list-item-inner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
 .checklist-title {
     font-size: 18px;
     font-weight: 500;
