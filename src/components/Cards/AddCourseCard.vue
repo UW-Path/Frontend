1267a4aaa0b20fe14067d7784daebdf5e2d4c09d
Adrian Santo Pasqualini <aspasqua@edu.uwaterloo.ca>
2020-08-28 22:12:52 -0400

    Fix UWFlow Link

--- src/components/Cards/AddCourseCard.vue
+++ src/components/Cards/AddCourseCard.vue
@@ -40,13 +40,13 @@
                   {{ selectedCourse.course_name}}
                 </v-card-subtitle>
                 <v-card-subtitle class="course-info-subheading">
-                  Credits: {{ selectedCourse.credit }} | ID: {{ selectedCourse.course_id }} | uwflow
+                  Credits: {{ selectedCourse.credit }} | ID: {{ selectedCourse.course_id }} | <a style="text-decoration:none" target="_blank" :href="'https://uwflow.com/course/' + selectedCourse.course_code.replace(/\s/g, '').toLowerCase()">UWFlow link</a>
                 </v-card-subtitle>
                 <v-card-text>{{ selectedCourse.info + (selectedCourse.offering === "" ? "" :  " Offered in: " + selectedCourse.offering.slice(0,-1) + ".") + (selectedCourse.online ? " Offered Online." : "")}}</v-card-text>
                 <v-card-text class="course-description-text">{{ "Credits: " + selectedCourse.credit }}</v-card-text>
-                <v-card-text class="course-description-text" v-if="selectedCourse.prereqs !== ''">{{ "Prerequisites: " + selectedCourse.prereqs }}</v-card-text>
-                <v-card-text class="course-description-text" v-if="selectedCourse.antireqs !== ''">{{ "Antirequisites: " + selectedCourse.antireqs }}</v-card-text>
-                <v-card-text class="course-description-text" v-if="selectedCourse.coreqs !== ''">{{ "Corequisites: " + selectedCourse.antireqs }}</v-card-text>
+                <v-card-text class="course-description-text" v-if="selectedCourse.prereqs && selectedCourse.prereqs.length > 0">{{ "Prerequisites: " + selectedCourse.prereqs }}</v-card-text>
+                <v-card-text class="course-description-text" v-if="selectedCourse.antireqs && selectedCourse.antireqs.length > 0">{{ "Antirequisites: " + selectedCourse.antireqs }}</v-card-text>
+                <v-card-text class="course-description-text" v-if="selectedCourse.coreqs && selectedCourse.coreqs.length > 0">{{ "Corequisites: " + selectedCourse.coreqs }}</v-card-text>
               </v-col>
             </v-row>
           </v-container>
