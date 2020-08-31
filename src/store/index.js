bcda02c4a62be60658e83dde0fe025173369b2d9
kevin feng <kfeng@quantstamp.com>
2020-08-30 20:56:37 -0700

    removed vue2touch deps

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
 import Vuex from "vuex";
 import Vue from "vue";
-import Vue2TouchEvents from 'vue2-touch-events'
 import courses from "./modules/courses";
 import programInfo from "./modules/programInfo"
 import CourseSelection from "./modules/courseSelection"
 
 Vue.use(Vuex);
-Vue.use(Vue2TouchEvents);
-
 
 export default new Vuex.Store({
     modules: {
