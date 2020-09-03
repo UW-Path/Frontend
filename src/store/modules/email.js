e380c4f6fd76f472970df2e3da5730beb80445bf
Hao Wei <weihuang0313@gmail.com>
2020-09-03 15:37:19 -0400

    send email button works with backend, bug where laurier not showing up in table

---
+++ src/store/modules/email.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// const state = {}
+// const getters = {}
+
+// Production Kubernetes API
+const backend_api = "";
+
+// Dev API
+// const backend_api = "http://127.0.0.1:8000";
+
+const actions = {
+    async sendEmail({commit},emailInfo){
+        var response = await axios.post(backend_api + "/api/send_email", emailInfo).catch(error => { void error; return null });
+        commit("Response", response)
+        return response
+        
+    }
+}
+
+export default {
+    actions
+}
