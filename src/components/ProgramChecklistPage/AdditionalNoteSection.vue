47b8e454a9665c1e002d656d69c4618909c3c033
Hao Wei <weihuang0313@gmail.com>
2020-12-31 17:32:13 -0500

    Add section for additional notes

---
+++ src/components/ProgramChecklistPage/AdditionalNoteSection.vue
@@ -0,0 +1,48 @@
+// This page is to list all the additional info based on different programs
+<template>
+    <span>
+    <p class="smallText">
+        <i>
+            * Please refer to the undergrad calendar for the most accurate information 
+            (click on the major/minor/option title). Note: Most plans need 20 credits to graduate. <br/>
+            If the checklist adds up to less than 20 credits, the remaning are assumed to be general electives.
+        </i>
+    </p>
+    <p class="smallText" v-if="program==='Bachelor of Computer Science'">
+        <i>
+            ** The electives must satisfy BCS breadth and depth requirements
+        </i>
+    </p>
+
+    <p class="smallText" v-if="program==='Mathematics/Financial Analysis and Risk Management'">
+        <i>
+            ** Must choose a specialization. Please refer to undergrad calendar for more details
+        </i>
+    </p>
+
+
+    <p class="smallText" v-if="program==='Bachelor of Arts'">
+        <i>
+            ** Each specific plan in Arts has its own unique requiremnts. Please refer to undergrad calendar for more details 
+        </i>
+    </p>
+    
+    </span>
+</template>
+
+<script>
+
+export default {
+    name: "AdditionalNoteSection",
+    props: {
+        program: String
+    },
+}
+</script>
+
+<style scoped>
+.smallText{
+    text-align: left 
+}
+
+</style>
