376a3794f4382ef6ab0749ffafda423d1dc73f40
Hao Wei <weihuang0313@gmail.com>
2021-01-15 07:35:00 -0500

    typo + clarification

--- src/components/ProgramChecklistPage/AdditionalNoteSection.vue
+++ src/components/ProgramChecklistPage/AdditionalNoteSection.vue
@@ -3,27 +3,32 @@
     <span>
     <p class="smallText">
         <i>
-            * Please refer to the <a :href="getLinkToUnderGradCalender" target="blank">undergrad calendar</a> for the most accurate information 
+            * The checklist is based on the most recent academic calendar year.
+        </i>
+    </p>
+    <p class="smallText">
+        <i>
+            ** Please refer to the <a :href="getLinkToUnderGradCalender" target="blank">undergrad calendar</a> for the most accurate information 
             (click on the major/minor/option title). Note: Most plans need 20 credits to graduate. <br/>
             If the checklist adds up to less than 20 credits, the remaning are assumed to be general electives.
         </i>
     </p>
     <p class="smallText" v-if="program==='Bachelor of Computer Science'">
         <i>
-            ** The electives must satisfy BCS breadth and depth requirements
+            *** The electives must satisfy BCS breadth and depth requirements.
         </i>
     </p>
 
     <p class="smallText" v-if="program==='Mathematics/Financial Analysis and Risk Management'">
         <i>
-            ** Must choose a specialization. Please refer to undergrad calendar for more details
+            *** Must choose a specialization. Please refer to undergrad calendar for more details.
         </i>
     </p>
 
 
     <p class="smallText" v-if="program==='Bachelor of Arts'">
         <i>
-            ** Each specific plan in Arts has its own unique requiremnts. Please refer to undergrad calendar for more details 
+            *** Each specific plan in Arts has its own unique requiremnts. Please refer to undergrad calendar for more details. 
         </i>
     </p>
     
