581665000736bfd46a17dbb4ed66ad56aa4a26a9
William-Gao1 <william2010.gao@gmail.com>
2022-09-17 21:51:07 -0400

    fix env reference

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 
 var firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY || "",
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN || "",
-  projectId: process.env.VUE_APP_PROJECT_ID || "",
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID || "",
-  appId: process.env.VUE_APP_APP_ID || "",
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID || ""
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY || "",
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: process.env.VUE_APP_APP_FIREBASE_ID || "",
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID || ""
 };
 
 firebase.initializeApp(firebaseConfig);
