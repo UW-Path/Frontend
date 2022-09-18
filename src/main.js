ff8bb34f17e324ff01606445552c5e464a38e629
William-Gao1 <william2010.gao@gmail.com>
2022-09-17 21:40:17 -0400

    fix env references

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 
 var firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: process.env.VUE_APP_API_KEY || "",
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN || "",
+  projectId: process.env.VUE_APP_PROJECT_ID || "",
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID || "",
+  appId: process.env.VUE_APP_APP_ID || "",
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID || ""
 };
 
 firebase.initializeApp(firebaseConfig);
